4acb79d05dea08d92fb5085a6bf653964fe18a1e
yusaristark <yusaristark@gmail.com>
2020-11-01 21:04:21 +0300

    static and singleton removed

--- AndroidFirstHomework/app/src/main/java/com/hse/androidfirsthomework/ColoredNumberRepository.java
+++ AndroidFirstHomework/app/src/main/java/com/hse/androidfirsthomework/ColoredNumberRepository.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class ColoredNumberRepository {
 
-    private static volatile ColoredNumberRepository mInstance;
+    /*private static volatile ColoredNumberRepository mInstance;
 
     public static ColoredNumberRepository getInstance() {
         if (mInstance == null) {
@@ -15,12 +15,12 @@ public class ColoredNumberRepository {
             }
         }
         return mInstance;
-    }
+    }*/
 
     protected final ArrayList<ColoredNumber> mData;
 
 
-    private ColoredNumberRepository() {
+    public ColoredNumberRepository() {
         mData = initializeData();
     }
 
